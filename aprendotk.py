dd989a28b2ea3b27f1c9f3bf24615ac6e7f2b438
mattgaviota <mattgaviota@gmail.com>
2010-10-03 15:01:54 -0300

    cambios

--- aprendotk.py
+++ aprendotk.py
@@ -15,62 +15,69 @@ class App:
         self.codarea = tk.StringVar()
         self.numlocal = tk.StringVar()
         self.mensaje = ''
-        imagen = Image.open("captcha.png")
-        self.photo = ImageTk.PhotoImage(imagen)
+        self.captcha = tk.StringVar()
         self.lenmax = 110 - len(self.remitente.get())
         
-        '''Boton para salir'''
-        self.button = tk.Button(self.frame, text="Salir", command=self.frame.quit)
-        self.button.grid(row = 2, column = 6)
+        self.mostrar_captcha()
+        
         
         '''Boton para enviar'''
         self.hi_there = tk.Button(self.frame, text="Enviar", command=self.send)
-        self.hi_there.grid(row = 2, column = 5)
+        self.hi_there.grid(row = 3, column = 4)
         
         '''Etiqueta del remitente'''
         self.remitente_label = tk.Label(self.frame, text = "Remitente")
-        self.remitente_label.grid(row = 1, column = 1)
+        self.remitente_label.grid(row = 2, column = 1)
         
         '''Caja de entrada para el remitente'''
-        self.ent_remitente = tk.Entry(self.frame, width = 10, textvariable = self.remitente)
-        self.ent_remitente.grid(row = 1, column = 2)
+        self.ent_remitente = tk.Entry(self.frame, width = 10, textvariable = self.remitente, borderwidth = 3)
+        self.ent_remitente.grid(row = 2, column = 2)
         
         '''Etiqueta del codigo de area'''
         self.cod_label = tk.Label(self.frame, text = "Codigo de area")
-        self.cod_label.grid(row = 1, column = 3)
+        self.cod_label.grid(row = 1, column = 1)
         
         '''Caja de entrada del codigo de area'''
-        self.ent_codarea = tk.Entry(self.frame, width = 4, textvariable = self.codarea)
-        self.ent_codarea.grid(row = 1, column = 4)
+        self.ent_codarea = tk.Entry(self.frame, width = 4, textvariable = self.codarea, borderwidth = 3)
+        self.ent_codarea.grid(row = 1, column = 2)
         
         '''Etiqueta del numero local'''
         self.num_label = tk.Label(self.frame, text = "Numero")
-        self.num_label.grid(row = 1, column = 5)
+        self.num_label.grid(row = 1, column = 3)
         
         '''Caja de entrada para el numero local'''
-        self.ent_numlocal = tk.Entry(self.frame, width = 7, textvariable = self.numlocal)
-        self.ent_numlocal.grid(row = 1, column = 6)
+        self.ent_numlocal = tk.Entry(self.frame, width = 7, textvariable = self.numlocal, borderwidth = 3)
+        self.ent_numlocal.grid(row = 1, column = 4)
         
         '''Etiqueta del mensaje'''
         self.msje_label = tk.Label(self.frame, text = "Mensaje")
         self.msje_label.grid(row = 2, column = 3)
         
-        self.ent_msje = tk.Text(self.frame, width=20, height=4)
+        '''Entrada de texto para el mensaje'''
+        self.ent_msje = tk.Text(self.frame, width=25, height=4, wrap = "word", borderwidth = 3)
         self.ent_msje.grid(row = 2, column = 4) 
         
         '''Etiqueta del captcha'''
         self.cap_label = tk.Label(self.frame, text = "Captcha")
-        self.cap_label.grid(row = 2, column = 1)
+        self.cap_label.grid(row = 3, column = 1)
         
-        '''Imagen del captcha'''
-        self.captcha_label = tk.Label(self.frame, image = self.photo)
-        self.captcha_label.photo = self.photo
-        self.captcha_label.grid(row = 2, column = 2)
+        '''Caja de entrada para el captcha'''
+        self.ent_captcha = tk.Entry(self.frame, width = 4, textvariable = self.captcha, borderwidth = 3)
+        self.ent_captcha.grid(row = 3, column = 3)
         
+                
     def send(self):
         self.mensaje =  self.ent_msje.get("1.0", tk.END)
         print self.mensaje
-
+    
+    def mostrar_captcha(self):
+        imagen = Image.open("captcha.png")
+        self.photo = ImageTk.PhotoImage(imagen)
+        '''Imagen del captcha'''
+        self.captcha_label = tk.Label(self.frame, image = self.photo)
+        self.captcha_label.photo = self.photo
+        self.captcha_label.grid(row = 3, column = 2)
+        
 def main():
     root = tk.Tk()
     root.title("SMS to Personal")
