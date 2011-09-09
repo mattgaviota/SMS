de86d15c3d976f160f0695a0ded57d595e5d54b5
mattgaviota <mattgaviota@gmail.com>
2011-09-08 22:58:59 -0300

    estilo arreglado

--- personal.py
+++ personal.py
@@ -6,32 +6,30 @@ import urllib
 
 from codecs import encode
 
-#Navegador 
 from browser import get_browser
 
 
 class Personal():
-    
+
     def __init__(self):
         self.browser = get_browser()
         self.path = """http://sms2.personal.com.ar/Mensajes/sms.php"""
-    
+
     def get_captcha(self):
             html = self.browser.get_html(self.path)
             match = re.search(r'(http://.*?tmp/.*?\.png)', html)
-    
+
             while (type(match) == type(None)) or (not match.group()):
                 html = self.browser.get_html(self.path)
                 match = re.search(r'(http://.*?tmp/.*?\.png)', html)
-    
+
             imageurl = match.group()
             imagepath = r'/tmp/captchalive.png'
             urllib.urlretrieve(imageurl, imagepath)
             return imagepath
 
-
     def send(self, number, captcha, message, sender):
-    
+
             form = self.browser.get_forms()[0]
             form.set_all_readonly(False)
             message = encode(message, 'latin-1', 'replace')
