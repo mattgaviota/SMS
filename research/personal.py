7f06a87c5a4412e51d7083f71d1e6303cacd0c44
Carlos Cabrera <point_to_null@esdebian.org>
2011-09-13 02:01:23 -0300

    prototipado de flujo

--- research/personal.py
+++ research/personal.py
@@ -1,11 +1,62 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
 
+from Queue import Queue, Empty, Full
 from litebrowser import Browser
+from threading import Thread
+
+
+class Asyncobj(Thread):
+    def __init__(self, func, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+        self.func = func
+        Thread.__init__(self)
+        self.result = None
+
+
+    def __call__(self):
+        return self
+
+
+    def is_alive(self):
+        try:
+            return Thread.is_alive(self)
+        except AttributeError:
+            return Thread.isAlive(self)
+
+
+    def run(self):
+        self.result = self.func(*self.args, **self.kwargs)
+
+
+    def get_result(self, timeout=None):
+        self.join(timeout)
+        return self.result
+
+
+class Async:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kw):
+        func = Asyncobj(self.func, *args, **kw)
+        func.start()
+        return func
+
+    def __repr__(self):
+        return self.func.func_name
+
 
 class Sender:
-    def __init__(callback=None):
+    def __init__(dst_num, message, captcha_ callback=None):
         self._callback = callback
+
+
+class Server:
+    def __init__(captcher, callback=None):
+        self._callback = callback
+
 
 def main():
     pass
