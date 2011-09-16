2ce1050c0ccdd51901b0f6ff5fd886b068318294
mattgaviota <mattgaviota@gmail.com>
2011-09-16 01:47:38 -0300

    pagina cambiada

--- personal.py
+++ personal.py
@@ -13,7 +13,7 @@ class Personal():
 
     def __init__(self):
         self.browser = get_browser()
-        self.path = """http://sms2.personal.com.ar/Mensajes/sms.php"""
+        self.path = """http://sms.personal.com.ar/Mensajes/sms.php"""
 
     def get_captcha(self):
         '''
